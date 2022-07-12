b5389e20c945bb90ef454904d737bbb1f9161ccb
sagarjain19 <jainsagar2324@gmail.com>
2022-07-12 16:52:17 +0530

    New.

--- code/snowflake/ddl/V1.1.6__CTO_TEST.sql
+++ code/snowflake/ddl/V1.1.6__CTO_TEST.sql
@@ -1,5 +1,5 @@
 USE SCHEMA EDW;
-CREATE TABLE EDW.CTO3
+CREATE TABLE EDW.CTO4
 (
    CTOID VARCHAR
   ,CTO_NAME VARCHAR
@@ -7,11 +7,11 @@ CREATE TABLE EDW.CTO3
 );
 
 
-insert into EDW.CTO3 Values (1, 'nitin','dell');
-insert into EDW.CTO3 Values (2, 'srikanth','dell');
-insert into EDW.CTO3 Values (3, 'nitin','dell');
-insert into EDW.CTO3 Values (4, 'srikanth','dell');
-insert into EDW.CTO3 Values (5, 'nitin','dell');
-insert into EDW.CTO3 Values (6, 'srikanth','dell');
-insert into EDW.CTO3 Values (7, 'nitin','dell');
-insert into EDW.CTO3 Values (8, srikanth','dell');
+insert into EDW.CTO4 Values (1, 'nitin','dell');
+insert into EDW.CTO4 Values (2, 'srikanth','dell');
+insert into EDW.CTO4 Values (3, 'nitin','dell');
+insert into EDW.CTO4 Values (4, 'srikanth','dell');
+insert into EDW.CTO4 Values (5, 'nitin','dell');
+insert into EDW.CTO4 Values (6, 'srikanth','dell');
+insert into EDW.CTO4 Values (7, 'nitin','dell');
+insert into EDW.CTO4 Values (8, srikanth','dell');
