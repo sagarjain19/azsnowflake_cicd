ba67d3d52ab92482e83e331b70981628a6dd9148
sagarjain19 <jainsagar2324@gmail.com>
2022-07-14 13:05:07 +0530

    Adding employee data.

---
+++ code/snowflake/ddl/V1.1.7__EmployeeData_TEST.sql
@@ -0,0 +1,17 @@
+USE SCHEMA EDW;
+CREATE TABLE EDW.EmployeeData
+(
+   Employee VARCHAR
+  ,Employee_NAME VARCHAR
+  ,Employee_COMPANY VARCHAR
+);
+
+
+insert into EDW.EmployeeData Values (1, 'nitin','dell');
+insert into EDW.EmployeeData Values (2, 'srikanth','dell');
+insert into EDW.EmployeeData Values (3, 'nitin','dell');
+insert into EDW.EmployeeData Values (4, 'srikanth','dell');
+insert into EDW.EmployeeData Values (5, 'nitin','dell');
+insert into EDW.EmployeeData Values (6, 'srikanth','dell');
+insert into EDW.EmployeeData Values (7, 'nitin','dell');
+insert into EDW.EmployeeData Values (8, 'srikanth','dell');
